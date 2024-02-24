792af3da92edfe321ac79ded36170dfaeba7215b
henrymutega <hmutega@gmail.com>
2024-02-24 23:17:45 +0300

    User input assignment commit.

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name please: ")
+
+age = input("What is your current age: ")
+
+location = input("What is your location: ")
+
+print("Hello " + name + " you are " + age + " years old. Plaesure to know you live at " + location)
